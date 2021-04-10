a8e00c76272b1cd757eec5963365e51e7a7d47c6
Игорь <vasyukovin@yandex.ru>
2021-04-10 16:25:20 +0300

    Создает компонент со списками фильмов

--- src/view/films-lists.js
+++ src/view/films-lists.js
@@ -2,19 +2,17 @@ export const createLists = () => {
   return `<section class="films">
     <section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-      <div class="films-list__container">
+      <div class="films-list__container" id="all-films">
       </div>
     </section>
     <section class="films-list films-list--extra">
       <h2 class="films-list__title">Top rated</h2>
-
-      <div class="films-list__container">
+      <div class="films-list__container" id="top-rated-films">
       </div>
     </section>
     <section class="films-list films-list--extra">
       <h2 class="films-list__title">Most commented</h2>
-
-      <div class="films-list__container">
+      <div class="films-list__container" id="most-commented-films">
       </div>
     </section>
   </section>`;
