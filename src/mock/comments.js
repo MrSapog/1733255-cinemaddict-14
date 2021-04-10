69692ee3baaa4de12c8824b3a94c72d588ed4c3b
Игорь <vasyukovin@yandex.ru>
2021-04-10 16:28:38 +0300

    Генерация случайного комментария

---
+++ src/mock/comments.js
@@ -0,0 +1,42 @@
+import {getRandomInteger} from '../utils.js';
+
+const COMMENTS_COUNT = 20;
+
+const emojis = [
+  './images/emoji/smile.png',
+  './images/emoji/sleeping.png',
+  './images/emoji/puke.png',
+  './images/emoji/angry.png',
+];
+
+const texts = [
+  'Interesting setting and a good cast',
+  'Booooooooooring',
+  'Very very old. Meh',
+  'Almost two hours? Seriously?',
+  'Never watch this!',
+  'Excellent',
+];
+
+const authors = [
+  'Tim',
+  'Nick',
+  'Rod',
+  'Todd',
+  'Miles',
+];
+
+const createRandomComment = () => {
+  return {
+    id: getRandomInteger(1, 1000),
+    emoji: emojis[getRandomInteger(0, emojis.length - 1)],
+    get emojiAlt() {
+      return this.emoji.match(/emoji\/(\w+)\.png/)[1];
+    },
+    text: texts[getRandomInteger(0, texts.length - 1)],
+    author: authors[getRandomInteger(0, authors.length - 1)],
+  };
+};
+
+export const randomComments = new Array(COMMENTS_COUNT).fill().map(() => createRandomComment());
+
